d707f1456d651bf491afc93b071227408fe9fcae
Nico Baier <nico.baier@gmail.com>
2023-09-27 00:01:04 -0500

    fix(index.ts): change Bun to process in the today variable assignment to fix undefined variable error feat(index.ts): add error handling for fetch request and display error message if response is not ok feat(index.ts): add check for empty usage object and exit the process if there is no usage data feat(index.ts): add error handling for fetch request and display error message if an error occurs

--- src/index.ts
+++ src/index.ts
@@ -10,40 +10,58 @@ import { DateTime } from "luxon";
 
 const timeZone = "America/Chicago";
 const date = DateTime.now();
-const today = Bun.argv.slice(2)[0] || date.toFormat("yyyy-MM-dd");
+const today = process.argv.slice(2)[0] || date.toFormat("yyyy-MM-dd");
 const month = date.toFormat("LLLL").toLowerCase();
 const now = date.toISO();
 
-const response = await fetch(`https://api.openai.com/v1/usage?date=${today}`, {
-  method: "GET",
-  headers: {
-    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-  },
-});
+try {
+  const response = await fetch(
+    `https://api.openai.com/v1/usage?date=${today}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+    }
+  );
 
-const { data, whisper_api_data, dalle_api_data } = (await response.json()) as {
-  data: TextModelUsage[];
-  whisper_api_data: AudioModelUsage[];
-  dalle_api_data: ImageModelUsage[];
-};
-
-const usage = calculateDayTotal(data, whisper_api_data, dalle_api_data);
-
-let totalCost = 0;
-
-for (const type in usage) {
-  const models = usage[type as keyof DayTotal];
-
-  for (const model in models) {
-    const modelData = models[model];
-    totalCost += modelData.cost;
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status} ${response.statusText}`);
   }
-}
 
-console.log(
-  `Total cost for ${today}:`,
-  totalCost.toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD",
-  })
-);
+  const { data, whisper_api_data, dalle_api_data } =
+    (await response.json()) as {
+      data: TextModelUsage[];
+      whisper_api_data: AudioModelUsage[];
+      dalle_api_data: ImageModelUsage[];
+    };
+
+  const usage = calculateDayTotal(data, whisper_api_data, dalle_api_data);
+
+  let totalCost = 0;
+
+  for (const type in usage) {
+    const models = usage[type as keyof DayTotal];
+
+    for (const model in models) {
+      const modelData = models[model];
+      totalCost += modelData.cost;
+    }
+  }
+
+  if (Object.keys(usage).length === 0) {
+    console.log("No usage on", today);
+    process.exit(0);
+  }
+
+  console.log(
+    `Total cost for ${today}:`,
+    totalCost.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    })
+  );
+} catch (error) {
+  // Handle the error here
+  console.error(error);
+}
